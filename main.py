20878025013f03fb8becfb8ce02946ef52464d64
gk <fakir56@mail.ru>
2023-06-01 20:14:10 +0300

    qwdqw

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-number = int(input('Введите номер дня\n'))
+#9.59
+# someString = input('Введите строку\n')
+# lenString = len(someString)
+# index = 0
+# cntSymbol = 0
+# while index < lenString:
+#     if someString[index] == 'o':
+#         cntSymbol += 1
+#     index += 1
+# print(f'Колличество символов "o" в строке: {cntSymbol}')
+import shlex
 
-if number % 7 == 1:
-    print('понедельник')
-    if number % 7 == 2:
-        print('вторник')
-    if number % 7 == 3:
-        print('среда')
-    if number % 7 == 4:
-        print('четверг')
-    if number % 7 == 5:
-        print('пятница')
-    if number % 7 == 6:
-        print('суббота')
-    if number % 7 == 0:
-        print('воскресенье')
+#9.76(b)
+# someString = input('Вводим строку\n')
+# index = 0
+# indexSymbol = 0
+# lenString = len(someString)
+# while index < lenString:
+#     if someString == 'e':
+#        indexSymbol = index
+#     index += 1
+# print(indexSymbol + 1)
+
+########################################################
+# Строки
+# sym = 'It\'s my house'
+# sym1 = 'It\'s my ' + sym
+# sym2 =f""""Строка 1
+# Строка 3
+# {5+6}
+# Строка 2 """""
+# print()
+
+# print('hi' * 5) - дублирует
+
+someString = 'hello world'
+print(someString[0])
+print(someString[-1])
