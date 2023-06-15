938639bef44550be90a9ca580f024bc65810ed1c
gk <fakir56@mail.ru>
2023-06-15 21:10:21 +0300

    qwdqw12

--- main.py
+++ main.py
@@ -116,3 +116,130 @@ import shlex
 #
 # print(minNumber)
 # print(maxNumber)
+
+
+############### 15.06.2023
+
+
+# numbers = list(map(int, input().split()))
+#
+# print(numbers)
+# numbers.append(12)
+# print(numbers)
+# numbers.pop(1)
+# print(numbers)
+# numbers.insert(1, 2)
+# print(numbers)
+# numbers.remove(15)
+# print(numbers)
+
+
+# someString = 'Hello World from main.py'
+# someList = someString.split(' ')
+# print(someList)
+
+
+# someList = ['Hello World from main.py']
+# someString = '+'.join(someList)
+# print(someString)
+
+# someList = ['Hello', 'World', 'from', 'main.py']
+#
+# for word in someList:
+#     print(word)
+
+
+# someString = 'Hello World from mainpy file'
+# someList = someString.split()
+# for word in someList:
+#     if len(word) > 4:
+#         print(word)
+
+# someString = 'Hello World from mainpy file'
+# someList = someString.split()
+# index = 0
+# for word in someList:
+#     if len(word) <=4:
+#         del someList[index]
+#     index += 1
+#
+# print(someList)
+
+
+# someString = 'Hello World from mainpy file'
+# someList = someString.split()
+# result = []
+# for word in someList:
+#     if len(word) > 4:
+#         result.append(word)
+# print(result)
+
+#11.73
+
+# somelist = [4, 7, 8, 3, 34, 15]
+# resultList1 = []
+# resultList2 = []
+#
+# for item in somelist:
+#     if item % 2 == 0:
+#         resultList1.append(item)
+#     if item % 10 == 5:
+#         resultList2.append(item)
+# print(resultList1)
+# print(resultList2)
+# print(len(resultList1))
+# print(len(resultList2))
+
+#11.82
+
+# someList = [4, 7, 8, 3, 34, 15]
+# result1 = []
+#
+# for item in someList:
+#     if item > 10:
+#         result1.append(item)
+# print(result1)
+# print(sum(result1) / len(result1))
+
+#11.197
+
+# someList = [34, 5, 6, 34, 78, 56, 12, 0, 8]
+# print(someList.count(2) != 0)
+
+
+# someRange = list(range(3, 10, 4))
+# print(someRange)
+
+# someList = ['hello', 'world', 'this', 'is', 'a', 'list']
+#
+# for index in range(len(someList)):
+#     print(index, someList[index])
+
+
+# 0 hello
+# 1 world
+# 2 this
+# 3 is
+# 4 a
+# 5 list
+#
+# Process finished with exit code 0
+
+#заменить четное число 0 !!!
+
+# someList = [34, 5, 6, 34, 78, 56, 12, 0, 8]
+# for index in range(len(someList)):
+#     if someList[index] % 2 == 0:
+#         someList[index] = 0
+# print(someList)
+
+
+#11.209
+
+someList = [1, 2, 3, 5, 2, 5, 4, 7]
+isTrue = True
+for index in range(len(someList)):
+    if someList[index] < someList[index -1]:
+        isTrue = False
+        break
+    print(index)
